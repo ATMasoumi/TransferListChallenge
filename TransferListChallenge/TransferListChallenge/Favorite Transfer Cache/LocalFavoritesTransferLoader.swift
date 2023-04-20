f4294955b19815d8758430f0ce527a86f9fcaadd
Amin <Torabi.dsd@gmail.com>
2023-04-20 20:01:23 +0330

    move code to production

---
+++ TransferListChallenge/TransferListChallenge/Favorite Transfer Cache/LocalFavoritesTransferLoader.swift
@@ -0,0 +1,68 @@
+//
+//  LocalFavoritesTransferLoader.swift
+//  TransferListChallenge
+//
+//  Created by Amir Masoumi on 4/20/23.
+//
+
+import Foundation
+
+public class LocalFavoritesTransferLoader: FavoritesTransferLoader {
+    
+    let store: FavoritesTransferStore
+    public init(store: FavoritesTransferStore) {
+        self.store = store
+    }
+    
+    public func load(completion: @escaping (LoadResult) -> Void) {
+        store.retrieve { result in
+            switch result {
+            case let .success(transfers):
+                completion(.success(transfers.toModels()))
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    public func save(_ transfer: TransferListChallenge.Transfer, completion: @escaping (FavoritesTransferLoader.SaveResult) -> Void) {
+        
+        let localTransfer = LocalTransfer(person: LocalPerson(fullName: transfer.person.fullName, email: transfer.person.email, avatar: transfer.person.avatar), card: LocalCard(cardNumber: transfer.card.cardNumber, cardType: transfer.card.cardType), lastTransfer: transfer.lastTransfer, note: transfer.note, moreInfo: LocalMoreInfo(numberOfTransfers: transfer.moreInfo.numberOfTransfers, totalTransfer: transfer.moreInfo.totalTransfer))
+        
+        store.insert(localTransfer) { result in
+            switch result {
+            case .success:
+                completion(.success(()))
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    public func delete(_ transfer: TransferListChallenge.Transfer, completion: @escaping (DeleteResult) -> Void) {
+       
+        let localTransfer = LocalTransfer(person: LocalPerson(fullName: transfer.person.fullName, email: transfer.person.email, avatar: transfer.person.avatar), card: LocalCard(cardNumber: transfer.card.cardNumber, cardType: transfer.card.cardType), lastTransfer: transfer.lastTransfer, note: transfer.note, moreInfo: LocalMoreInfo(numberOfTransfers: transfer.moreInfo.numberOfTransfers, totalTransfer: transfer.moreInfo.totalTransfer))
+        
+        store.delete(localTransfer) { result in
+            switch result {
+            case .success:
+                completion(.success(()))
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+}
+
+private extension Array where Element == LocalTransfer {
+    func toModels() -> [Transfer] {
+        return map {
+            let person = Person(fullName: $0.person.fullName, email: $0.person.email, avatar: $0.person.avatar)
+            let card = Card(cardNumber: $0.card.cardNumber, cardType: $0.card.cardType)
+            let moreInfo = MoreInfo(numberOfTransfers: $0.moreInfo.numberOfTransfers, totalTransfer: $0.moreInfo.totalTransfer)
+            let item = Transfer(person: person, card: card, lastTransfer: $0.lastTransfer, note: $0.note, moreInfo: moreInfo)
+           return item
+        }
+    }
+}
---
+++ TransferListChallenge/TransferListChallenge/Favorite Transfer Cache/LocalTransfer.swift
@@ -0,0 +1,54 @@
+//
+//  LocalTransfer.swift
+//  TransferListChallenge
+//
+//  Created by Amir Masoumi on 4/20/23.
+//
+
+import Foundation
+
+// MARK: - Transfer
+public struct LocalTransfer: Equatable {
+    public let person: LocalPerson
+    public let card: LocalCard
+    public let lastTransfer: Date
+    public let note: String?
+    public let moreInfo: LocalMoreInfo
+    public init(person: LocalPerson, card: LocalCard, lastTransfer: Date, note: String?, moreInfo: LocalMoreInfo) {
+        self.person = person
+        self.card = card
+        self.lastTransfer = lastTransfer
+        self.note = note
+        self.moreInfo = moreInfo
+    }
+}
+
+// MARK: - Card
+public struct LocalCard: Equatable {
+    public let cardNumber, cardType: String
+    public init(cardNumber: String, cardType: String) {
+        self.cardNumber = cardNumber
+        self.cardType = cardType
+    }
+}
+
+// MARK: - MoreInfo
+public struct LocalMoreInfo: Equatable {
+    public let numberOfTransfers, totalTransfer: Int
+    public init(numberOfTransfers: Int, totalTransfer: Int) {
+        self.numberOfTransfers = numberOfTransfers
+        self.totalTransfer = totalTransfer
+    }
+}
+
+// MARK: - Person
+public struct LocalPerson: Equatable {
+    public let fullName: String
+    public let email: String?
+    public let avatar: URL
+    public init(fullName: String, email: String?, avatar: URL) {
+        self.fullName = fullName
+        self.email = email
+        self.avatar = avatar
+    }
+}
