d36fe64987f44ee87e647a74604cd95e731a8b5c
Amin <Torabi.dsd@gmail.com>
2023-04-20 16:25:56 +0330

    load requests cache retrieval is empty at first

--- TransferListChallenge/TransferListChallengeTests/Transfer Cache/CacheTransferUseCaseTests.swift
+++ TransferListChallenge/TransferListChallengeTests/Transfer Cache/CacheTransferUseCaseTests.swift
@@ -6,28 +6,69 @@
 //
 
 import XCTest
+import TransferListChallenge
 
-protocol FavoritesTransferStore {
+struct LocalTransfer: Equatable {
     
 }
 
-class LocalFavoritesTransferLoader {
+protocol FavoritesTransferLoader {
+    typealias LoadResult = Swift.Result<[Transfer], Error>
+    func load(completion: @escaping (LoadResult) -> Void)
+}
+
+protocol FavoritesTransferStore {
+    typealias RetrievalResult = Result<[LocalTransfer]?, Error>
+    func retrieve(completion: @escaping (RetrievalResult) -> Void)
+}
+
+class LocalFavoritesTransferLoader: FavoritesTransferLoader {
+    
     let store: FavoritesTransferStore
     init(store: FavoritesTransferStore) {
         self.store = store
+    }
+    
+    func load(completion: @escaping (LoadResult) -> Void) {
+        store.retrieve { result in
+            
+        }
     }
 }
 
 final class CacheTransferUseCaseTests: XCTestCase {
 
     func test_init_doesNotMessageStoreUponCreation() throws {
-        let store = FavoritesTransferStoreSpy()
-        let _ = LocalFavoritesTransferLoader(store: store)
-        
+        let (_, store) = makeSUT()
         XCTAssertEqual(store.receivedMessages, [])
     }
     
+    func test_load_requestsCacheRetrieval() {
+        let (sut, store) = makeSUT()
+        
+        sut.load { _ in }
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve([])])
+    }
+    
+    // MARK: - Helpers
+    
+    func makeSUT() -> (LocalFavoritesTransferLoader,FavoritesTransferStoreSpy) {
+        let store = FavoritesTransferStoreSpy()
+        let sut = LocalFavoritesTransferLoader(store: store)
+        return (sut, store)
+    }
+    
     class FavoritesTransferStoreSpy: FavoritesTransferStore {
-        var receivedMessages: [String] = []
+       
+        enum ReceivedMessage: Equatable {
+            case retrieve([LocalTransfer])
+        }
+        
+        var receivedMessages: [ReceivedMessage] = []
+        
+        func retrieve(completion: @escaping (RetrievalResult) -> Void) {
+            receivedMessages.append(.retrieve([]))
+        }
     }
 }
