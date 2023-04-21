50c7e70b375e2307475612f2af6c6cff8845e9e4
Amin <Torabi.dsd@gmail.com>
2023-04-21 14:23:38 +0330

    Initializing unit test for developing platform agnostic 'TransferViewModel'

---
+++ TransferListChallenge/TransferListChallenge/Presentation /TransferViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TransferViewModel.swift
+//  TransferListChallenge
+//
+//  Created by Amir Masoumi on 4/21/23.
+//
+
+import Foundation
---
+++ TransferListChallenge/TransferListChallengeTests/Presentation/TransferViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  TransferViewModelTests.swift
+//  TransferListChallengeTests
+//
+//  Created by Amir Masoumi on 4/21/23.
+//
+
+import XCTest
+
+final class TransferViewModelTests: XCTestCase {
+
+    func testExample() throws {
+        XCTFail("Test Failed Successfully")
+    }
+    
+}
