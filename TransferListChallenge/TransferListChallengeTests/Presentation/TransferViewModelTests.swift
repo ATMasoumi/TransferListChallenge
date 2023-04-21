b3d8271acc01d6e983dfd9ed87b3c110f3de56e0
Amin <Torabi.dsd@gmail.com>
2023-04-21 21:56:29 +0330

    test delivers transfers on load call

--- TransferListChallenge/TransferListChallengeTests/Presentation/TransferViewModelTests.swift
+++ TransferListChallenge/TransferListChallengeTests/Presentation/TransferViewModelTests.swift
@@ -6,11 +6,69 @@
 //
 
 import XCTest
+import TransferListChallenge
+
+class TransferViewModel: ObservableObject {
+    
+    @Published var transfers: [Transfer] = []
+    
+    let transferLoader: TransferLoader
+    init(transferLoader: TransferLoader) {
+        self.transferLoader = transferLoader
+    }
+    
+    func load() {
+        transferLoader.load { result in
+            switch result {
+            case let .success(transfers):
+                self.transfers = transfers
+            case .failure:
+                break
+            }
+        }
+    }
+}
 
 final class TransferViewModelTests: XCTestCase {
 
-    func testExample() throws {
-        XCTFail("Test Failed Successfully")
+    func test_doesNotLoad_onInit() throws {
+        let transferLoader = TransferLoaderSpy()
+        let sut = TransferViewModel(transferLoader: transferLoader)
+        XCTAssertEqual(sut.transfers, [])
+    }
+    
+    func test_deliversTransfers_onLoadCall() throws {
+        let transfer = makeItem(name: "amin", cardNumber: "1", note: "note").model
+        let transferLoader = TransferLoaderSpy()
+        let sut = TransferViewModel(transferLoader: transferLoader)
+        sut.load()
+        transferLoader.complete(with: [transfer])
+        XCTAssertEqual(sut.transfers, [transfer])
+    }
+    
+    class TransferLoaderSpy: TransferLoader {
+        var transferCompletions: [(TransferLoader.Result) -> Void] = []
+        
+        func load(completion: @escaping (TransferLoader.Result) -> Void) {
+            transferCompletions.append(completion)
+        }
+        
+        func complete(with transfers: [Transfer]) {
+            guard let completion = transferCompletions.first else { return }
+            completion(.success(transfers))
+        }
+    }
+    
+    private func makeItem(name: String, cardNumber: String, note: String) -> (model: Transfer, local: LocalTransfer) {
+        let person = Person(fullName: name, email: "Torabi.dsd@gmail.com", avatar: URL(string: "any-url.com")!)
+        let card = Card(cardNumber: cardNumber, cardType: "Personal")
+        let transferDate : String = "2022-07-14T02:47:58Z"
+        let moreInfo = MoreInfo(numberOfTransfers: 5, totalTransfer: 100)
+        let item = Transfer(person: person, card: card, lastTransfer: transferDate.toDate(), note: note, moreInfo: moreInfo)
+        
+        
+        let localItem = LocalTransfer(person: LocalPerson(fullName: person.fullName, email: person.email, avatar: person.avatar), card: LocalCard(cardNumber: card.cardNumber, cardType: card.cardType), lastTransfer: transferDate.toDate(), note: note, moreInfo: LocalMoreInfo(numberOfTransfers: moreInfo.numberOfTransfers, totalTransfer: moreInfo.totalTransfer))
+        return (item, localItem)
     }
     
 }
