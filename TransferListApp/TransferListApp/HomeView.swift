5cb4fea306c66e7ce2359f91f067830c5bb5b509
Amin <Torabi.dsd@gmail.com>
2023-04-22 20:25:39 +0330

    Added remote transfer list to prototype

--- TransferListApp/TransferListApp/HomeView.swift
+++ TransferListApp/TransferListApp/HomeView.swift
@@ -10,18 +10,47 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         VStack {
-            favoritesTitle
+            title(name: "Favorites")
             favoritesItems
-            Spacer()
+            title(name: "All")
+            transferList
         }
     }
     
+    var transferList: some View {
+        List {
+            ForEach(1...10, id: \.self) { item in
+                transferListCell(url: "", title: "name ", subTitle: "subtitle")
+                    .listRowSeparator(.hidden)
+            }
+        }
+        .listStyle(.plain)
+        .ignoresSafeArea()
+    }
+    func transferListCell(url: String, title: String, subTitle: String) -> some View {
+        HStack {
+            Circle()
+                .frame(width: 60, height: 60)
+            VStack (alignment: .leading, spacing: 10){
+                Text(title)
+                    .bold()
+                Text(subTitle)
+                    .foregroundColor(.secondary)
+            }
+            Spacer()
+            Image(systemName: "star.fill")
+                .foregroundColor(.yellow)
+            Image(systemName: "chevron.right")
+                .foregroundColor(Color(UIColor.tertiaryLabel))
+        }
+//        .padding()
+    }
     
     var favoritesItems: some View {
         ScrollView(.horizontal) {
             HStack {
                 ForEach(1...5, id: \.self) { item in
-                    favoritesCell(url: "", title: "\(item) name ", subTitle: "\(item)")
+                    favoritesCell(url: "", title: "name ", subTitle: "subtitle")
                 }
             }
             .padding(.leading)
@@ -40,12 +69,14 @@ struct HomeView: View {
         .padding()
     }
   
-    var favoritesTitle: some View {
+    func title(name: String) -> some View {
         HStack {
-            Text("Favorites")
-                .font(.largeTitle)
+            Text(name)
+                .font(.title)
+                .bold()
             Spacer()
         }
+        .padding(.leading)
     }
 }
 
