ef237cc4b9f2dacd9703ac7ffb9917030b732067
Amin <Torabi.dsd@gmail.com>
2023-04-22 19:25:37 +0330

    Added new app workSpace

---
+++ TransferListApp/TransferListApp/TransferListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransferListAppApp.swift
+//  TransferListApp
+//
+//  Created by Amir Masoumi on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct TransferListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
